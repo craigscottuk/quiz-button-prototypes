60e1be958c0158ccd23abf10187271c3ecdadaff
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2022-07-10 19:03:19 +0200

    Progress

--- main.js
+++ main.js
@@ -10,4 +10,12 @@ function answBtnUpDown(event) {
   }
 }
 
+const appHeight = () => {
+  const doc = document.documentElement;
+  doc.style.setProperty('--app-height', `${window.innerHeight}px`);
+};
+
+window.addEventListener('resize', appHeight);
+appHeight();
+
 document.addEventListener('click', answBtnUpDown);
